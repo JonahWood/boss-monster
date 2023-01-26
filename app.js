4fa3397b69247dd8790f24e53dd3bd954d61bf01
Jonah <jonah.osito@icloud.com>
2023-01-26 14:27:26 -0700

    goro attack good

--- app.js
+++ app.js
@@ -14,19 +14,37 @@ const heroes = [
 ]
 
 
-const goro = {
+const boss = {
+    name: 'Goro',
     health: 100,
     maxHealth: 100,
     damage: 5,
     level: 1,
 }
 
-function goroAttackBoth(){
-    let goroAttackBoth = heroes[0];
-    console.log(heroes);
+function findHero(heroName){
+    let foundHero = heroes.find(h => heroes.name == heroName)
+
+
+
 }
-function goroAttackSingle(){
-    let 
+console.log(findHero())
+
+
+
+
+
+
+
+function goroAttackBoth(heroName){
+    let hero = heroes.find(h => h.name == heroName);
+    hero.health -= 5
+
+    console.log(hero);
 }
 
-goroAttackBoth()
+
+function goroAttackSingle(heroHealth){
+    let lessHealth = heroes.find(h => h.health == heroesHealth)
+}
+// goroAttackBoth()
