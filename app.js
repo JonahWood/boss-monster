aaa46564b4254fc2579423198d94af734e5dfd73
Jonah <jonah.osito@icloud.com>
2023-01-26 13:40:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const heroes = [
+    {
+        name: 'Johnny Cage',
+        type: 'Human',
+        damage: 10,
+        health: 50,
+        image: "url(https://upload.wikimedia.org/wikipedia/en/7/7b/Johnny_Render_mk_11.png)"
+    },
+    {
+        name: 'Jax',
+        type: 'Cyborg',
+        damage: 5,
+        health: 100,
+        image: "url(https://upload.wikimedia.org/wikipedia/en/4/4c/Jax_mk11.png)"
+    }
+]
+
+
+const goro = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1,
+    image: "url(https://upload.wikimedia.org/wikipedia/en/f/f7/Goro_%28Mortal_Kombat%29.png)"
+}
